59d589ca26b7778f4157f46a1950c387ad3ec759
Dmitriy Belonogov <belonogoff90@gmail.com>
2021-01-03 22:24:28 +0300

    step_5

--- src/main/java/ru/belonogoff/springmvc/controllers/FirstController.java
+++ src/main/java/ru/belonogoff/springmvc/controllers/FirstController.java
@@ -1,29 +1,50 @@
 package ru.belonogoff.springmvc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(HttpServletRequest request) {
-        String name = request.getParameter("name");
-        String surname = request.getParameter("surname");
-        System.out.println("Hello, " + name + " " + surname);
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+        model.addAttribute("message", "Hello, " + name + " " + surname);
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
-    public String goodByePage(@RequestParam(value = "name", required = false) String name,
-                              @RequestParam(value = "surname", required = false) String surname) {
-        System.out.println("Goodbye, " + name + " " + surname);
+    public String goodByePage() {
         return "first/goodbye";
     }
 
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam(value = "a", required = false) int a,
+                             @RequestParam(value = "b", required = false) int b,
+                             @RequestParam(value = "action", required = false) String action,
+                             Model model) {
+        switch (action) {
+            case "multiplication":
+                model.addAttribute("result", a * b);
+                break;
+            case "addition":
+                model.addAttribute("result", a + b);
+                break;
+            case "subtraction":
+                model.addAttribute("result", a - b);
+                break;
+            case "division":
+                model.addAttribute("result", a / (double) b);
+                break;
+            default:
+                model.addAttribute("result", "Invalid action");
+                break;
+        }
+        return "first/calculator";
+    }
 }
